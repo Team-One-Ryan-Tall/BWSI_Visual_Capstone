85150306ab98c4bb735065d4cb813d80a064a9b8
Boueny Folefack <bouenyfolefack@gmail.com>
2022-07-21 18:24:46 -0500

    fixed bounded_box.py

---
+++ BoundedBox.py
@@ -0,0 +1,31 @@
+
+
+import matplotlib.pyplot as plt
+from camera import take_picture
+import numpy as np
+from facenet_models import FacenetModel
+from matplotlib.patches import Rectangle
+from LoadImage import load_image
+
+def detect(model: FacenetModel,filepath: str):
+    return model.detect(load_image(filepath))
+
+def detect_arr(model: FacenetModel,img: np.ndarray):
+    return model.detect(img)
+
+def display_boxes(model: FacenetModel, filepath: str):
+    fig, ax = plt.subplots()
+    img = load_image(filepath)
+    ax.imshow(img)
+    boxes, probabilities, landmarks = model.detect(img)
+
+    for box, prob, landmark in zip(boxes, probabilities, landmarks):
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="red"))
+
+def display_boxes_arr(model: FacenetModel, img: np.ndarray):
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    boxes, probabilities, landmarks = model.detect(img)
+
+    for box, prob, landmark in zip(boxes, probabilities, landmarks):
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="red"))
